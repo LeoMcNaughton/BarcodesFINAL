ffdce4d478702405f122e05331236df6c4a066e7
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-12-08 14:43:06 -0600

    Add files via upload

---
+++ BookFactory.js
@@ -0,0 +1,15 @@
+const ISBN = require("./ISBN.js");
+const EAN = require("./EAN.js");
+
+const BookFactory = function (){
+
+
+    function createBook(pubYear){
+        //create this function
+        
+    }
+
+    return {createBook};
+}
+
+module.exports = BookFactory;
